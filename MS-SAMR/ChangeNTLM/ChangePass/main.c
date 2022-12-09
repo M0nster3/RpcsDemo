b4d58d37bb6666331ad1d86671d47a231daf8183
M0nster3 <1571531203@qq.com>
2022-12-10 01:46:05 +0800

    update

--- MS-SAMR/ChangeNTLM/ChangePass/main.c
+++ MS-SAMR/ChangeNTLM/ChangePass/main.c
@@ -24,6 +24,11 @@ NTSTATUS WINAPI RtlEncryptLmOwfPwdWithLmOwfPwd(IN LPCBYTE DataLmOwfPassword, IN 
 #define STATUS_PASSWORD_RESTRICTION 0xC000006C
 #define STATUS_MORE_ENTRIES 0x00000105L
 NTSTATUS status = STATUS_DATA_ERROR;
+
+unsigned char oldNT[16];
+unsigned char newNT[16];
+unsigned char newLM[16];
+
 BOOL StringToHex(IN LPCWCHAR string, IN LPBYTE hex, IN DWORD size)
 {
 	DWORD i, j;
@@ -104,76 +109,134 @@ PSAMPR_SERVER_NAME_unbind()
 }
 
 NTSTATUS __RPC_USER
-PASSWORD(RPC_UNICODE_STRING* ServerName, RPC_UNICODE_STRING* UserName)
+PASSWORD(RPC_UNICODE_STRING* ServerName, RPC_UNICODE_STRING* UserName, unsigned char* oldNT, unsigned char* newNT)
 {
+	SAMPR_HANDLE hServer, hDomain, hUser;
 	int i;
-	unsigned char oldNT[16];
-	unsigned char newNT[16];
-	//unsigned char newLM[16];
+	
+	long enumDomainStatus, changePassStatus;
+	
+	
+	ENCRYPTED_NT_OWF_PASSWORD NewLm;
+	ENCRYPTED_LM_OWF_PASSWORD OldNt;
+	ENCRYPTED_NT_OWF_PASSWORD NewNT;
+	unsigned long domainEnumerationContext = 0;
+	unsigned long domainCountReturned;
+	PSAMPR_ENUMERATION_BUFFER pEnumDomainBuffer = NULL;
+	SAMPR_REVISION_INFO inRevisionInfo, outRevisionInfo;
+	unsigned long outVersion;
+	PRPC_SID domainSID = 0;
+	SAMPR_ULONG_ARRAY ridBuffer = { 0, NULL };
+	SAMPR_ULONG_ARRAY useBuffer = { 0, NULL };
+	unsigned long RID = 0;
 
-	PCWCHAR oldNTLM = L"2c0f5d06fcaee5abe033dc69c0626ce1";
-	PCWCHAR newNTLM = L"2c0f5d06fcaee5abe033dc69c0626ce1";
-	StringToHex(oldNTLM, oldNT, sizeof(oldNT));
-	StringToHex(newNTLM, newNT, sizeof(newNT));
-	ENCRYPTED_NT_OWF_PASSWORD NewPasswordEncryptedWithOldLm;
-	ENCRYPTED_LM_OWF_PASSWORD OldLmOwfPasswordEncryptedWithNewNt;
-	ENCRYPTED_NT_OWF_PASSWORD NewLMEncryptedWithNewNT;
-
-	status = RtlEncryptLmOwfPwdWithLmOwfPwd(oldNT, newNT, &NewPasswordEncryptedWithOldLm);
-	if (!NT_SUCCESS(status))
+	status = SamrConnect5(ServerName, SAM_SERVER_CONNECT | SAM_SERVER_ENUMERATE_DOMAINS | SAM_SERVER_LOOKUP_DOMAIN, 1, &inRevisionInfo, &outVersion, &outRevisionInfo, &hServer);
+	if (!NT_SUCCESS(status) && hServer != NULL)
 	{
-		wprintf(L"[!] Calc OldNtEncryptedWithNewNt Error: %08X\n", status);
+		wprintf(L"[!] SamrConnect Error: %08X\n", status);
 		exit(1);
 	}
-	status = RtlEncryptLmOwfPwdWithLmOwfPwd(newNT, oldNT, &OldLmOwfPasswordEncryptedWithNewNt);
-	if (!NT_SUCCESS(status))
-	{
-		wprintf(L"[!] Calc NewNtEncryptedWithOldNt Error: %08X\n", status);
-		exit(1);
-	}
-	PSAMPR_ENCRYPTED_USER_PASSWORD NewPasswordEncryptedWithOldNt=L"test123..";
-	PENCRYPTED_NT_OWF_PASSWORD OldNtOwfPasswordEncryptedWithNewNt=L"test123..";
 
-	status=SamrUnicodeChangePasswordUser2(
-		PSAMPR_SERVER_NAME_bind(), 
-		ServerName,
-		UserName, 
-		&NewPasswordEncryptedWithOldNt, 
-		&OldNtOwfPasswordEncryptedWithNewNt,
-		FALSE, 
-		NULL,
-		NULL
-		);
-	if (NT_SUCCESS(status))
+	do
 	{
-		wprintf(L"[*] Change password success! :)\n");
-	}
-	else if (status == STATUS_WRONG_PASSWORD)
-	{
-		wprintf(L"[!] Wrong Password!\n");
-	}
-	else if (status == STATUS_PASSWORD_RESTRICTION)
-	{
-		wprintf(L"[!] Restriction!\n");
-	}
-	else
-	{
-		wprintf(L"[!] Error Code: %08X\n", status);
-	}
+		enumDomainStatus = SamrEnumerateDomainsInSamServer(hServer, &domainEnumerationContext, &pEnumDomainBuffer, 100, &domainCountReturned);
+		if (!NT_SUCCESS(enumDomainStatus) && enumDomainStatus != STATUS_MORE_ENTRIES)
+		{
+			wprintf(L"[!] SamrEnumerateDomainsInSamServer Wrong 0x%ld\n", status);
+			exit(1);
+		}
+		const wchar_t* sBuiltin = L"Builtin";
+		for (i = 0; i < domainCountReturned; i++) {
+			if (wcscmp(sBuiltin, pEnumDomainBuffer->Buffer[i].Name.Buffer) == 0)
+				continue;
+
+			status = SamrLookupDomainInSamServer(hServer, &pEnumDomainBuffer->Buffer[i].Name, &domainSID);
+			if (!NT_SUCCESS(status)) {
+				wprintf(L"SamrLookupDomainInSamServer Wrong");
+				exit(1);
+			}
+			
+			status = SamrOpenDomain(hServer, DOMAIN_LOOKUP, domainSID, &hDomain);
+			if (!NT_SUCCESS(status)) {
+				wprintf(L"SamrOpenDomain Wrong");
+				exit(1);
+			}
+
+
+			status = SamrLookupNamesInDomain(hDomain, 1, &UserName[0], &ridBuffer, &useBuffer);
+			if (!NT_SUCCESS(status)) {
+				wprintf(L"SamrLookupNamesInDomain Wrong");
+				exit(1);
+			}else{
+				RID = ridBuffer.Element[0];
+				wprintf(L"[*] SamrLookupNamesInDomain  RID: %d\n", RID);
+			}
+
+			
+			status = SamrOpenUser(hDomain, USER_CHANGE_PASSWORD, RID, &hUser);
+			status = RtlEncryptLmOwfPwdWithLmOwfPwd(oldNT, newNT, &NewNT);
+			if (!NT_SUCCESS(status))
+			{
+				wprintf(L"[!] Calc OldNtEncryptedWithNewNt Error: %08X\n", status);
+				exit(1);
+			}
+			status = RtlEncryptLmOwfPwdWithLmOwfPwd(newNT, oldNT, &OldNt);
+			if (!NT_SUCCESS(status))
+			{
+				wprintf(L"[!] Calc NewNtEncryptedWithOldNt Error: %08X\n", status);
+				exit(1);
+			}
+			/*PSAMPR_ENCRYPTED_USER_PASSWORD NewPasswordEncryptedWithOldNt = L"test123..";
+			PENCRYPTED_NT_OWF_PASSWORD OldNtOwfPasswordEncryptedWithNewNt = L"test123..";*/
+			
+			PCWCHAR newLMHash = "AAD3B435B51404EEAAD3B435B51404EE";
+			StringToHex(newLMHash, newLM, sizeof(newLM));
+			status = RtlEncryptLmOwfPwdWithLmOwfPwd(newLM, newNT, &NewLm);
+			if (!NT_SUCCESS(status))
+			{
+				wprintf(L"[!] Calc NewLMEncryptedWithNewNT Error: %08X\n", status);
+				exit(1);
+			}
+			status = SamrChangePasswordUser(hUser, FALSE, NULL, NULL, TRUE, &OldNt, &NewNT, FALSE, NULL, TRUE, &NewLm);
+			if (NT_SUCCESS(status))
+			{
+				wprintf(L"[*] Change password success!\n");
+				exit(1);
+			}
+			else if (status == STATUS_WRONG_PASSWORD)
+			{
+				wprintf(L"[!] Wrong Password!\n");
+			}
+			else if (status == STATUS_PASSWORD_RESTRICTION)
+			{
+				wprintf(L"[!] Restriction!\n");
+			}
+			else
+			{
+				wprintf(L"[!] Error Code: %08X\n", status);
+			}
+		}
+
+	} while (enumDomainStatus == STATUS_MORE_ENTRIES);
+	
 }
 
 int main()
 {
 
-	wchar_t username[] = L"Monster";
+	wchar_t username[] = L"M0nster3";
 	wchar_t servicenames[] = L"MONSTERA834";
+	PCWCHAR oldNTLM = L"2c0f5d06fcaee5abe033dc69c0626ce3";
+	PCWCHAR newNTLM = L"2c0f5d06fcaee5abe033dc69c0626ce4";
+	StringToHex(oldNTLM, oldNT, sizeof(oldNT));
+	StringToHex(newNTLM, newNT, sizeof(newNT));
 	unsigned short servicenameLen = sizeof(servicenames) - 2;
 	RPC_UNICODE_STRING servicename[] = { servicenameLen, 30, servicenames };
 
 	unsigned short usernameLen = sizeof(username) - 2;
 	RPC_UNICODE_STRING userName[] = { usernameLen, 30, username };
 
-	PASSWORD(&servicename, &userName);
+	PASSWORD(&servicename, &userName, oldNT, newNT);
 
 
 	return 0;
